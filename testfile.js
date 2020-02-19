7c3920d2cf4796963a4eb498686d0121efd66c8d
Paul <paultje52@gmail.com>
2020-02-19 21:25:59 +0100

    Removed token :D
    I allready resetted the token, so try as you wish, won’t work ;-)

--- testfile.js
+++ testfile.js
@@ -1,6 +1,6 @@
 const BananenBase = require("./src/index.js");
 
-new BananenBase("NDA3NTM5NzgyOTU3MjY4OTky.XkhH1g.BPCDz889aXf5DKibEkpNaEmfYz8")
+new BananenBase("TOKEN")
   .addModule("loader", {
     commands: "commands",
     events: "events"
