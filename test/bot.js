7575932fd5450b65fb0d012568ac4f1619edaac6
Paultje52 <paultje52@gmail.com>
2020-07-11 14:51:55 +0200

    Added database module
    Co-authored-by: ChickenDevv <chickendevv@gmail.com>

--- test/bot.js
+++ test/bot.js
@@ -2,7 +2,19 @@ const BananenBase = require("../src/BananenBase");
 
 new BananenBase("TOKEN")
   .setConfig(require("./config.json"))
-  .addModule(["loader", "alias"])
+  .addModule(["loader", "alias", "database"])
+  .addModule("security", {
+    defaultPermissions: {
+      user: ["SEND_MESSAGES"],
+      bot: ["EMBED_LINKS"]
+    },
+    permissionNames: {
+      botOwner: (message, BananenBase) => {
+        let botOwners = ["327462385361092621", "259776081316282368"];
+        return botOwners.includes(message.author.id);
+      }
+    }
+  })
   .ready((BananenBase) => {
     BananenBase.start();
   });
