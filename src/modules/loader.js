bfd856d184c1c0c18f9b5cad2e15df6ba661c392
zaid450 <me@zaid-arshad.tech>
2020-07-23 14:00:15 -0400

    Updated loader.js

--- src/modules/loader.js
+++ src/modules/loader.js
@@ -3,12 +3,11 @@ const colors = require("../colors.js");
 module.exports = class LoaderModule extends require("../constructors/module.js") {
   constructor() {
     super({
-      dependencies: ["fsscanner"],
       name: "loader",
       toConfigure: {
-        commands: "optional.string", 
-        events: "optional.string", 
-        process_events: "optional.string", 
+        commands: "optional.string",
+        events: "optional.string",
+        process_events: "optional.string",
         functions: "optional.string"
       }
     });
@@ -25,19 +24,21 @@ module.exports = class LoaderModule extends require("../constructors/module.js")
       res(true);
     });
   }
-
   getFiles(folder) {
-    return new Promise((res) => {
-      const fsscanner = require("fsscanner");
-      const fs = require("fs");
-      if (!fs.existsSync(`${process.cwd()}/${folder}`)) return res([]);
-      fsscanner.scan(`${process.cwd()}/${folder}`, [fsscanner.criteria.pattern(".js"), fsscanner.criteria.type("F")], (err, results) => { 
-        if (err) throw new Error(`FsScanner error: Error while scanning ${process.cwd()}/${folder}!\n${err}`);
-        res(results);
-      });
+    return new Promise(async (res) => {
+      const { existsSync } = require('fs')
+      if (!existsSync(`${process.cwd()}/${folder}`)) return res([]);
+      res(await this.loadFiles(`${process.cwd()}/${folder}`));
     });
   }
-
+  async loadFiles(dir) {
+    const dirents = await require('fs').promises.readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(dirents.map((dirent) => {
+      const res = require("path").resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    }));
+    return Array.prototype.concat(...files);
+  }
   loadCommands(folder) {
     return new Promise(async (res) => {
       let files = await this.getFiles(folder);
@@ -59,7 +60,7 @@ module.exports = class LoaderModule extends require("../constructors/module.js")
           file.dir = name;
           if (!file.help.name) return console.warn(`${colors("[Loader]").cyan().done()} Command "${name}" doesn't have a name, so it won't be activated!`);
           this.BananenBase.commands[file.help.name] = file;
-        } catch(e) {
+        } catch (e) {
           console.log(`${colors("[Loader]").cyan().done()} Error while loading command "${name}"!\n${e}`);
         }
       }
@@ -85,7 +86,7 @@ module.exports = class LoaderModule extends require("../constructors/module.js")
           this.BananenBase.client.on(file.event, (...args) => {
             file.run(...args);
           });
-        } catch(e) {}
+        } catch (e) {}
       }
       console.log(`${colors("[Loader]").cyan().done()} ${files.length} event(s) loaded!`);
       res(true);
